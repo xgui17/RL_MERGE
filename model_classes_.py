5ca9db43d7a43892a51e05dfb58a838be050d6ba
xgui <xgui@ucsd.edu>
2024-11-04 14:59:15 -0800

    add comments

--- model_classes_.py
+++ model_classes_.py
@@ -285,17 +285,3 @@ class GCN_classification(torch.nn.Module):
         predicted = torch.argmax(softmax, 1)
         
         return softmax, predicted
-
-
-class MLPBaseline(nn.Module):
-    def __init__(self, input_dim, hidden_dim1, hidden_dim2, output_dim):
-        super(MLPBaseline, self).__init__()
-        self.layer1 = nn.Linear(input_dim, hidden_dim1)
-        self.layer2 = nn.Linear(hidden_dim1, hidden_dim2)
-        self.layer3 = nn.Linear(hidden_dim2, output_dim)
-        
-    def forward(self, x):
-        x = F.relu(self.layer1(x))
-        x = F.relu(self.layer2(x))
-        x = self.layer3(x)
-        return x
